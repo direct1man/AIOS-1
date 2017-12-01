dbdd23dbec418d9f76cd1f3feabd5e460e413891
direct1man <33375789+direct1man@users.noreply.github.com>
2017-12-01 19:58:15 +0300

    test 4

---
+++ simple_agent_templates/Watson/watson test.py
@@ -0,0 +1,9 @@
+#start_of_parameters
+#key=p1;  type=constant;  value=p1_value
+#end_of_parameters
+import pandas as pd
+import numpy as np
+
+col_definition1 = "{random_dict}"
+col1 = col_definition1.split("|")[0]
+file1 = col_definition1.split("|")[1]
